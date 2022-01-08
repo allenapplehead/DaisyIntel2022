f0139d8d7cbcdbd0c39f37230d44276554a5ac93
allenapplehead <allentao7@gmail.com>
2022-01-08 17:21:27 -0500

    Got linear transforms to work. Just need to tune a little bit

--- camera.py
+++ camera.py
@@ -2,6 +2,35 @@
 import cv2
 import numpy as np
 
+""" Basement measurements
+Coordinates in the real world
+A (0cm, 0cm)
+B (246cm, 0cm)
+C (156cm, 184cm)
+D (26.5cm, 184cm)
+Coordinates in the camera feed
+A (797, 598)
+B (75, 480)
+C (450, 317)
+D (721, 343)
+"""
+
+# Coordinates from basement
+quad_coords = {
+    "lonlat": np.array([
+        [0, 0], # Bottom right
+        [246, 5], # Bottom left
+        [154, 179], # Top left
+        [25, 173] # Top right
+    ]),
+    "pixel": np.array([
+        [797, 598], # Bottom right
+        [75, 480], # Bottom left
+        [450, 317], # Top left
+        [721, 343] # Top right
+    ])
+}
+
 # Pixel mapper from camera feed to longitutde latitude
 class PixelMapper(object):
     """
@@ -70,19 +99,43 @@ VID_WIDTH = 800
 VID_HEIGHT = 600
 
 while(True):
-	
-	# Capture the video frame
-	# by frame
-	ret, frame = vid.read()
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
 
-	# Display the resulting frame
-	cv2.imshow('frame', cv2.resize(frame, (VID_WIDTH, VID_HEIGHT)))
-	
-	# the 'q' button is set as the
-	# quitting button you may use any
-	# desired button of your choice
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    # Get vid dimensions
+    ORIG_WIDTH = vid.get(3)
+    ORIG_HEIGHT = vid.get(4)
+
+    # Get scaling ratios
+    SW = ORIG_WIDTH / VID_WIDTH
+    SH = ORIG_HEIGHT / VID_HEIGHT
+
+    # Draw in alignment points for 2D perspective transform from feed to floor
+    cv2.circle(frame, (int(797 * SW), int(598 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    cv2.circle(frame, (int(75 * SW), int(480 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    cv2.circle(frame, (int(450 * SW), int(317 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    cv2.circle(frame, (int(721 * SW), int(343 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    
+    pm = PixelMapper(quad_coords["pixel"], quad_coords["lonlat"])
+
+    uv_0 = (350, 400)
+    lonlat_0 = pm.pixel_to_lonlat(uv_0)
+    x = uv_0[0] * SW
+    y = uv_0[1] * SH
+    tx = lonlat_0[0][0] * SW
+    ty = lonlat_0[0][1] * SH
+    # Test out the transformation
+    cv2.circle(frame, (int(x), int(y)), radius=2, color=(0, 255, 0), thickness=-1)
+    print("Transformed [cm]:", tx, ty)
+    # Display the resulting frame
+    cv2.imshow('frame', cv2.resize(frame, (VID_WIDTH, VID_HEIGHT)))
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # After the loop release the cap object
 vid.release()
