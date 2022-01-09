3f94884d577a1b967cbf91a9bd6244c6cf2a06de
allenapplehead <allentao7@gmail.com>
2022-01-09 02:44:16 -0500

    Got mapping and tf object detection to work together. This allows the pixel locations of objects detected to be translated into real world measurements based on initial environment calibration

--- main.py
+++ main.py
@@ -5,13 +5,98 @@ from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
 from object_detection.utils import config_util
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# CONSTANTS
+VID_WIDTH = 800
+VID_HEIGHT = 600
+
+# Coordinates from basement
+quad_coords = {
+    "lonlat": np.array([
+        [0, 0], # Bottom right
+        [246, 5], # Bottom left
+        [154, 179], # Top left
+        [25, 173] # Top right
+    ]),
+    "pixel": np.array([
+        [797, 598], # Bottom right
+        [75, 480], # Bottom left
+        [450, 317], # Top left
+        [721, 343] # Top right
+    ])
+}
+
+# Pixel mapper from camera feed to longitutde latitude
+class PixelMapper(object):
+    """
+    Create an object for converting pixels to geographic coordinates,
+    using four points with known locations which form a quadrilteral in both planes
+    Parameters
+    ----------
+    pixel_array : (4,2) shape numpy array
+        The (x,y) pixel coordinates corresponding to the top left, top right, bottom right, bottom left
+        pixels of the known region
+    lonlat_array : (4,2) shape numpy array
+        The (lon, lat) coordinates corresponding to the top left, top right, bottom right, bottom left
+        pixels of the known region
+    """
+    def __init__(self, pixel_array, lonlat_array):
+        assert pixel_array.shape==(4,2), "Need (4,2) input array"
+        assert lonlat_array.shape==(4,2), "Need (4,2) input array"
+        self.M = cv2.getPerspectiveTransform(np.float32(pixel_array),np.float32(lonlat_array))
+        self.invM = cv2.getPerspectiveTransform(np.float32(lonlat_array),np.float32(pixel_array))
+        
+    def pixel_to_lonlat(self, pixel):
+        """
+        Convert a set of pixel coordinates to lon-lat coordinates
+        Parameters
+        ----------
+        pixel : (N,2) numpy array or (x,y) tuple
+            The (x,y) pixel coordinates to be converted
+        Returns
+        -------
+        (N,2) numpy array
+            The corresponding (lon, lat) coordinates
+        """
+        if type(pixel) != np.ndarray:
+            pixel = np.array(pixel).reshape(1,2)
+        assert pixel.shape[1]==2, "Need (N,2) input array" 
+        pixel = np.concatenate([pixel, np.ones((pixel.shape[0],1))], axis=1)
+        lonlat = np.dot(self.M,pixel.T)
+        
+        return (lonlat[:2,:]/lonlat[2,:]).T
+    
+    def lonlat_to_pixel(self, lonlat):
+        """
+        Convert a set of lon-lat coordinates to pixel coordinates
+        Parameters
+        ----------
+        lonlat : (N,2) numpy array or (x,y) tuple
+            The (lon,lat) coordinates to be converted
+        Returns
+        -------
+        (N,2) numpy array
+            The corresponding (x, y) pixel coordinates
+        """
+        if type(lonlat) != np.ndarray:
+            lonlat = np.array(lonlat).reshape(1,2)
+        assert lonlat.shape[1]==2, "Need (N,2) input array" 
+        lonlat = np.concatenate([lonlat, np.ones((lonlat.shape[0],1))], axis=1)
+        pixel = np.dot(self.invM,lonlat.T)
+        
+        return (pixel[:2,:]/pixel[2,:]).T
+
+
 # Load pipeline config and build a detection model
 configs = config_util.get_configs_from_pipeline_file("my_ssd_mobnet/pipeline.config")
 detection_model = model_builder.build(model_config=configs['model'], is_training=False)
 
 # Restore checkpoint
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join("my_ssd_mobnet", 'ckpt-3')).expect_partial()
+ckpt.restore(os.path.join("my_ssd_mobnet", 'ckpt-5')).expect_partial()
 
 @tf.function
 def detect_fn(image):
@@ -20,15 +105,15 @@ def detect_fn(image):
     detections = detection_model.postprocess(prediction_dict, shapes)
     return detections
 
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-
 category_index = label_map_util.create_category_index_from_labelmap("my_ssd_mobnet/label_map.pbtxt")
 
 cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Get scaling ratios
+SW = width / VID_WIDTH
+SH = height / VID_HEIGHT
 
 while cap.isOpened(): 
     ret, frame = cap.read()
@@ -48,7 +133,7 @@ while cap.isOpened():
     label_id_offset = 1
     image_np_with_detections = image_np.copy()
 
-    viz_utils.visualize_boxes_and_labels_on_image_array(
+    coords = viz_utils.visualize_boxes_and_labels_on_image_array(
                 image_np_with_detections,
                 detections['detection_boxes'],
                 detections['detection_classes']+label_id_offset,
@@ -59,7 +144,35 @@ while cap.isOpened():
                 min_score_thresh=.8,
                 agnostic_mode=False)
 
-    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+    # Draw in alignment points for 2D perspective transform from feed to floor
+    cv2.circle(image_np_with_detections, (int(797 * SW), int(598 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    cv2.circle(image_np_with_detections, (int(75 * SW), int(480 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    cv2.circle(image_np_with_detections, (int(450 * SW), int(317 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    cv2.circle(image_np_with_detections, (int(721 * SW), int(343 * SH)), radius=2, color=(0, 0, 255), thickness=-1)
+    
+    # Initialize pixel mapper
+    pm = PixelMapper(quad_coords["pixel"], quad_coords["lonlat"])
+
+    # Display the real world location on the floor for each detected item
+    for item in coords:
+        # Item: [name, xmin, ymin, xmax, ymax]
+        # Find the centroid of the bounding box
+        name, xmin, ymin, xmax, ymax = item[0], item[1], item[2], item[3], item[4]
+        centroid_x = (xmin + xmax) / 2
+        centroid_y = (ymin + ymax) / 2
+        # Map this to the real world
+        tmp = pm.pixel_to_lonlat((centroid_x, centroid_y))
+        vid_x = int(centroid_x * SW)
+        vid_y = int(centroid_y * SH)
+        real_x = int(tmp[0][0] * SW)
+        real_y = int(tmp[0][1] * SH)
+        # Display the transformed centroid on the bounding box
+        label = name + " IRL Pos (cm): " + str(real_x) + " " + str(real_y)
+        cv2.putText(image_np_with_detections, label, (vid_x, vid_y+50),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+    
+    # Display the resulting frame
+    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (VID_WIDTH, VID_HEIGHT)))
     
     if cv2.waitKey(10) & 0xFF == ord('q'):
         cap.release()
